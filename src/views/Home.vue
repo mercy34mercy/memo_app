3e54322f26fbf700eacfa63c25b88f670f24cf65
makun <etukobamasatyan@gmail.com>
2022-02-15 00:29:15 +0900

    ログイン機能とpost,pull実装

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,7 +5,13 @@
     <a v-for="task in tasks" :key="task">{{task}}</a>
   </div>
   <div>
-    <input v-bind="inputtask">
+    <input v-model="this.inputtask">
+  </div>
+  <div>
+    <button @click="fetchtasks()">いけ!</button>
+  </div>
+    <div>
+    <button @click="logingoogle()">ログイン</button>
   </div>
 </template>
 
@@ -13,13 +19,15 @@
 // @ is an alias to /src
 import { collection, doc, getDocs ,addDoc} from 'firebase/firestore';
 import { db } from "../main";
+import { getAuth, signInWithPopup,GoogleAuthProvider } from "firebase/auth";
 
 export default {
   name: 'Home',
   data :()=> {
     return{
       tasks:[],
-      inputtask:""
+      inputtask:"",
+      userid:""
     }
   },
   components: {
@@ -30,7 +38,7 @@ export default {
   methods: {
   async getCities() {
   // const citiesCol = collection(db, 'cities');
-  const citySnapshot = await getDocs(collection(db, "cities"));
+  const citySnapshot = await getDocs(collection(db, "tasks"));
   citySnapshot.forEach((doc)=>{
     this.tasks.push({...doc.data(),id:doc.id});
     console.log({...doc.data(),id:doc.id})
@@ -40,11 +48,38 @@ export default {
 async fetchtasks(){
     const docRef = await addDoc(collection(db, "tasks"), {
     task: this.inputtask,
-    id:1
+    userid: this.userid
 });
 console.log("Document written with ID: ", docRef.id);
 
+  },
+  logingoogle(){
+    const provider = new GoogleAuthProvider();
+    const auth = getAuth();
+    signInWithPopup(auth, provider)
+  .then((result) => {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    // The signed-in user info.
+    const user = result.user;
+    this.userid = user.uid
+    console.log(token,user)
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+    console.log(errorCode,errorMessage,email,credential)
+  });
   }
   }
 }
 </script>
+
+
